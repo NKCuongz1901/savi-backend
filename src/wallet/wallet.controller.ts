25441582e7e808f09b9ed7c3ba3ea562648215d4
nkhaccuong <cuong19012002@gmail.com>
2025-09-24 23:45:22 +0700

    feat: add get wallet controller

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  getWallet(@Request() req:any) {
+    const userId = req.user.userId;
+    return this.walletService.getWallet(userId);
+  }
 
   
 }
